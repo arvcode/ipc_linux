496dde31ccaf1c55ada2da477830d0ce18bbdfbd
Arvindh <srinivasan.arvindh@sg.panasonic.com>
2017-12-13 14:07:19 +0800

    Indent code

--- forke.c
+++ forke.c
@@ -1,40 +1,40 @@
- #include<stdio.h>
+#include<stdio.h>
 #include<signal.h>
 #include<string.h>
 sig_atomic_t count=0;
 
 void signal_handler(int signal) {
- count++;
+	count++;
 }
 
-main() {
+int main() {
 
-char *arglist []={"ls","-l","."};
-char *arglist1[]={"ps"};
-struct sigaction sigact;
-long int ret=0;
+	char *arglist []={"ls","-l","."};
+	char *arglist1[]={"ps"};
+	struct sigaction sigact;
+	long int ret=0;
 
-memset(&sigact, 0, sizeof(sigact));
-sigact.sa_handler=&signal_handler;
-sigaction(SIGINT,&sigact,NULL);
-while(ret<100000000) {ret++;}
-printf("count is %d \n",count);
-ret=fork();
-if (ret!=0) {
-	nice(1001);
-	printf("parent");
+	memset(&sigact, 0, sizeof(sigact));
+	sigact.sa_handler=&signal_handler;
+	sigaction(SIGINT,&sigact,NULL);
+	while(ret<100000000) {ret++;}
+	printf("count is %d \n",count);
+	ret=fork();
+	if (ret!=0) {
+		nice(1001);//Add nice -this process gets less priority 
+		printf("parent");
         execvp("ps",arglist1);
-	fflush(stdout);
-} else {
- nice(-100); 
-  system("ps");
-  printf("child");
-  fflush(stdout);
-  execvp("ls",arglist);
+		fflush(stdout);
+	} else {
+		nice(-100); 
+  		system("ps");
+ 	 	printf("child");
+  		fflush(stdout);
+  		execvp("ls",arglist);
 
-}
+	}	
 
-
+	return 0;
 
 
 
