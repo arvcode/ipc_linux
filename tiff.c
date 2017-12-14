c8f5f62dacc70bc2b8a63ade5d74e67ba8699d93
Arvindh <srinivasan.arvindh@sg.panasonic.com>
2017-12-14 18:10:12 +0800

    Updated GPL for tiff

--- tiff.c
+++ tiff.c
@@ -1,3 +1,9 @@
+/* GPL
+ * Taken from Advanced Linux Programming
+ * The source code contained in this book is covered by the GNU General Public License
+   (GPL), which is listed in Appendix F,.GNU General Public License.
+ */
+
 #include <stdio.h>
 #include <tiffio.h>
 int main (int argc, char** argv)
