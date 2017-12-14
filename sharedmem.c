ff036c2ec0011d8bf2d0c324dfd7edc8b2a84ac5
Arvindh <srinivasan.arvindh@sg.panasonic.com>
2017-12-14 18:02:35 +0800

    updated shared memory, thread kill

--- sharedmem.c
+++ sharedmem.c
@@ -2,6 +2,9 @@
 #include<sys/shm.h>
 #include<sys/stat.h>
 
+/* ipcs to show
+ * ipcrm -m to remove  */
+
 int main() {
 
 	int shm_id=0;
@@ -26,7 +29,7 @@ int main() {
 	shared_memory=(char*)shmat(shm_id, (void *)0x05000000,0);
 
 	printf("%s \n",shared_memory);
-
+	while(1);
 	shmdt(shared_memory);
 
 	shmctl(shm_id,IPC_RMID,0);	
--- th.c
+++ th.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<semaphore.h>
+#include<signal.h>
 int a=0;
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
+
+sem_t semap;
+
+pthread_cond_t cv;
+
+pthread_t gtid;
 
 typedef struct th_args {
 	int count;
@@ -17,18 +25,34 @@ void *handler (void * arg) {
 		printf("%s \n",t1->string);	
 		fflush(stdout);
 	}
+	sem_wait(&semap);
+//	sem_trywait(&semap);
+	sem_getvalue(&semap,&i);
 	pthread_mutex_lock(&mutex);
 	pthread_mutex_trylock(&mutex);	
 	a=a+1;
+	sem_post(&semap);
+	pthread_cond_wait(&cv,&mutex);
+	pthread_kill(gtid,SIGKILL);
 	pthread_mutex_unlock(&mutex); //RECURSIVE
 	pthread_mutex_unlock(&mutex);
 	
 }
 
+void *thread2 (void *arg) {
+printf("Operation V \n");
+pthread_mutex_lock(&mutex);
+sem_post(&semap);
+//pthread_cond_signal(&cv);
+pthread_cond_broadcast(&cv);
+pthread_mutex_unlock(&mutex);
+
+}
+
 int main(char **argv, int argc) {
     int a=0;
     th_args_t t1,t2;
-	pthread_t tid,tid1;
+	pthread_t tid,tid1,tid2;
 	pthread_attr_t attr;
 
 	pthread_attr_init(&attr);
@@ -41,18 +65,23 @@ int main(char **argv, int argc) {
 	pthread_mutex_init(&mutex,&mut_attr);
 	pthread_mutexattr_destroy(&mut_attr);
 
-	
-	t1.count=100000;
-	t1.string="test";
-	t2.count=20000;
-	t2.string="code";
+	sem_init(&semap,0,0);
 
+	pthread_cond_init(&cv,NULL);
+
+	t1.count=10;
+	t1.string="test";
+	t2.count=20;
+	t2.string="code";
+	gtid=tid1;
 	pthread_create(&tid,NULL,&handler,(void*)&t1);
 	pthread_create(&tid1,&attr,&handler,(void*)&t2);
+	pthread_create(&tid2,&attr,&thread2,NULL);
 	printf("print main thread \n");
 	pthread_attr_destroy(&attr);
 	pthread_join(tid,NULL);
 	pthread_join(tid1,NULL);
+	pthread_join(tid2,NULL);
  	pthread_detach(tid);	
-
+	sem_destroy(&semap);
 }
